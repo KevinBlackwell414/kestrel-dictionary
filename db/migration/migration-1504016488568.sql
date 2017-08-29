9d600f1a9153ee40b03bced8556ce0e3e67975be
kevinblackwell414 <kevinblackwell414@gmail.com>
2017-08-29 11:32:02 -0400

    Created migration and seed files

---
+++ db/migration/migration-1504016488568.sql
@@ -0,0 +1,14 @@
+CREATE TABLE IF NOT EXISTS users(
+    id BIGSERIAL PRIMARY KEY,
+    email VARCHAR(255),
+    user_name VARCHAR(255),
+    password VARCHAR(255)
+);
+
+CREATE TABLE IF NOT EXISTS dictionary(
+    id BIGSERIAL PRIMARY KEY,
+    word VARCHAR(255),
+    definition VARCHAR(1024),
+    usage VARCHAR(1024),
+    user_id INTEGER
+)
---
+++ db/seeds/users.sql
@@ -0,0 +1,12 @@
+INSERT INTO users(email, user_name)
+VALUES
+('daniel@kestral.com', 'DWeezyInTheTrap'),
+('gary@kestral.com', 'GareBear'),
+('stephanie@kestral.com', 'Nickanie'),
+('lenora@kestral.com', 'CrazyArtTeacher'),
+('yarden@kestral.com', 'neverDeadass'),
+('john@kestral.com', 'JesusSandals'),
+('issac@kestral.com', 'allBusiness'),
+('michael@kestral.com', 'SuperSmartyPants'),
+('amisha@kestral.com', 'CSMaster'),
+('nital@kestral.com', 'NitNatTitTat')
